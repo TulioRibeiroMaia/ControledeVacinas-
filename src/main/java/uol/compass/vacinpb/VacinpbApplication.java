7e340600c5ba7bfe9e0b5ca6eb0ceeaa7e6671ea
Pedro Amorim <amorimp@pm.me>
2022-01-04 20:15:10 -0300

    add initial spring boot project

---
+++ src/main/java/uol/compass/vacinpb/VacinpbApplication.java
@@ -0,0 +1,13 @@
+package uol.compass.vacinpb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacinpbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VacinpbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uol/compass/vacinpb/VacinpbApplicationTests.java
@@ -0,0 +1,13 @@
+package uol.compass.vacinpb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacinpbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
